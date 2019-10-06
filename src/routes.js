70191f94438d886b9dc36233f5ed792810c3abb0
César Augusto Moro  Fürst <cesaramfurst@gmail.com>
2019-10-06 19:17:07 -0300

    Criação de rotas e página login melhorada

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,16 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import Dashborad from "./pages/Dashborad";
+import Dashboard from "./pages/Dashboard";
 import New from "./pages/New";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact componemt={Login} />
-        <Route path="/dashborad" componemt={Dashborad} />
-        <Route path="/new" componemt={New} />
+        <Route path="/" exact component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/new" component={New} />
       </Switch>
     </BrowserRouter>
   );
